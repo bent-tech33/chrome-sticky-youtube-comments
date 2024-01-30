50ffa8b90edbe151185e653e2d8cd1e27c77ea70
Abrar Aamer <abraraamer1@gmail.com>
2024-01-30 17:45:26 +0300

    fix for full screen mode controls and chapter selection.

--- comments.js
+++ comments.js
@@ -6,91 +6,78 @@ function docReady(fn) {
     } else {
         document.addEventListener("DOMContentLoaded", fn);
     }
-}    
+}
 
-docReady(function() {
-	var is_sticky = false;
-	let buttonSticky = 'Sticky Comments';
-	let buttonUnSticky = 'UnSticky';
-	var intervalComments = null;
-	var stickyButton = null;
-	var comments = null;
-	var scrollProgress = false;
-        var commentholder = false;
-	chrome.storage.local.get('sticky', function(data) {
-		is_sticky = data.sticky ===true ? true: false;
-		
-		intervalComments = setInterval(waitForComments, 300);
-		
-});
-	
-	function updteWidth(){
-		var chapters = document.querySelectorAll(".ytp-chapters-container .ytp-chapter-hover-container");
-		for (var i=0, max=chapters.length; i < max; i++) {
-			var tempWidth = parseInt(chapters[i].style.width);
-			chapters[i].style.width = (tempWidth - (tempWidth * 0.25))+'px';
-			//-25% of what it is to compensate for the total browser width loss
-			console.log('old width: '+tempWidth+' new width:'+chapters[i].style.width );
-		}
-	}
-	
-	
-	function makeSticky(){
-		stickyButton.setAttribute('data-sticky',1);
-		stickyButton.classList.add('tfu-active');
-		stickyButton.textContent = buttonUnSticky;
-		document.querySelector("body").classList.add('tfu-sticky-comments');
-		window.dispatchEvent(new Event('resize'));//let youtube adjust player height/width
+docReady(function () {
+    var is_sticky = false;
+    let buttonSticky = 'Sticky Comments';
+    let buttonUnSticky = 'UnSticky';
+    var intervalComments = null;
+    var stickyButton = null;
+    var comments = null;
+    var commentholder = false;
+    chrome.storage.local.get('sticky', function (data) {
+        is_sticky = data.sticky === true ? true : false;
 
-	}
-	function makeUnSticky(){
-		stickyButton.setAttribute('data-sticky',0);
-		stickyButton.classList.remove('tfu-active');
-		stickyButton.textContent = buttonSticky;
-		document.querySelector("body").classList.remove('tfu-sticky-comments');
-		window.dispatchEvent(new Event('resize'));//let youtube adjust player height/width
-	}
-	function waitForComments(){
-		comments = document.querySelector('#comments');
-		if(comments !== null && comments !== undefined){
-			clearInterval(intervalComments);
-			comments = document.querySelector('#comments');
+        intervalComments = setInterval(waitForComments, 300);
 
-			//check if sticky or not
-			
-			comments.insertAdjacentHTML('beforebegin','<div id="tfu-comments-holder"></div>');
+    });
+
+    function makeSticky() {
+        stickyButton.setAttribute('data-sticky', 1);
+        stickyButton.classList.add('tfu-active');
+        stickyButton.textContent = buttonUnSticky;
+        document.querySelector("body").classList.add('tfu-sticky-comments');
+        window.dispatchEvent(new Event('resize'));//let youtube adjust player height/width
+
+    }
+    function makeUnSticky() {
+        stickyButton.setAttribute('data-sticky', 0);
+        stickyButton.classList.remove('tfu-active');
+        stickyButton.textContent = buttonSticky;
+        document.querySelector("body").classList.remove('tfu-sticky-comments');
+        window.dispatchEvent(new Event('resize'));//let youtube adjust player height/width
+    }
+    function waitForComments() {
+        comments = document.querySelector('#comments');
+        if (comments !== null && comments !== undefined) {
+            clearInterval(intervalComments);
+            comments = document.querySelector('#comments');
+
+            //check if sticky or not
+
+            comments.insertAdjacentHTML('beforebegin', '<div id="tfu-comments-holder"></div>');
             commentholder = document.querySelector('#tfu-comments-holder');
-			commentholder.appendChild(comments);
-			comments.insertAdjacentHTML('beforebegin', '<div id="tfu-comments-message"><a href="#" id="tfu-sticky-unsticky-comments" data-sticky="'+(is_sticky ? 1 : 0)+'">'+(is_sticky ? buttonUnSticky : buttonSticky)+'</a></div>');
-			stickyButton = document.querySelector('#tfu-sticky-unsticky-comments');
-			if(is_sticky){
-				makeSticky();
-			}
-			stickyButton.addEventListener('click', function(e){
-				e.preventDefault();
-				if(stickyButton.getAttribute('data-sticky')==0){
-					makeSticky();
-				}
-				else{
-					makeUnSticky();
-				}	
-			});
-			
-			
-			//attach scroll event to comments.
-			commentholder.addEventListener('scroll', function(e) {
-				//grace distance before reaching the end 
-				const grace = 50;
-				const bottom = e.target.scrollHeight - e.target.scrollTop - grace <= e.target.clientHeight;
-				if (bottom) {
-					//trigger scroll on the player so the youtube loads the comments.
-						document.getElementsByTagName("ytd-watch-flexy")[0].dispatchEvent(new CustomEvent('scroll'));
-						
-				}
-			});
-			
-			
-		}
-		
-	}
+            commentholder.appendChild(comments);
+            comments.insertAdjacentHTML('beforebegin', '<div id="tfu-comments-message"><a href="#" id="tfu-sticky-unsticky-comments" data-sticky="' + (is_sticky ? 1 : 0) + '">' + (is_sticky ? buttonUnSticky : buttonSticky) + '</a></div>');
+            stickyButton = document.querySelector('#tfu-sticky-unsticky-comments');
+            if (is_sticky) {
+                makeSticky();
+            }
+            stickyButton.addEventListener('click', function (e) {
+                e.preventDefault();
+                if (stickyButton.getAttribute('data-sticky') == 0) {
+                    makeSticky();
+                } else {
+                    makeUnSticky();
+                }
+            });
+
+
+            //attach scroll event to comments.
+            commentholder.addEventListener('scroll', function (e) {
+                //grace distance before reaching the end 
+                const grace = 50;
+                const bottom = e.target.scrollHeight - e.target.scrollTop - grace <= e.target.clientHeight;
+                if (bottom) {
+                    //trigger scroll on the player so the youtube loads the comments.
+                    document.getElementsByTagName("ytd-watch-flexy")[0].dispatchEvent(new CustomEvent('scroll'));
+
+                }
+            });
+
+
+        }
+
+    }
 });
